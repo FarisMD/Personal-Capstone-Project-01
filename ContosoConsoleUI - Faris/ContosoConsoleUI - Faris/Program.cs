76a8e94c96e1d9d8148f3005c1bf8ec9d8a04bd7
FarisMD <75193893+FarisMD@users.noreply.github.com>
2020-12-20 11:10:04 +0800

    Add files via upload

---
+++ ContosoConsoleUI - Faris/ContosoConsoleUI - Faris/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using ContosoModel;
+using System.Linq;
+using ContosoDataAccess;
+
+namespace ContosoConsoleUI___Faris
+{
+    class Program
+    {
+        #region declaration for course name
+        //static Course course = new Course(); - move to the method below
+        /* static List<Course> courses = new List<Course>(); - don't need store in list anymore
+        cause storing in sql server*/
+        #endregion
+
+        #region declaration for students
+        static Student student = new Student();
+        static List<Student> students = new List<Student>();
+        #endregion
+
+        static char enter;
+
+        static void Main(string[] args)
+        {
+
+            DisplayOptions();
+
+            while (enter != '5')
+            {
+                if (enter == '1')
+                {
+                    AddNewCourse();
+                }
+                if (enter == '2')
+                {
+                    AddStudent();
+                }
+                if (enter == '4')
+                {
+                    DisplayOptionfour();
+                }
+                if (enter == '3')
+                {
+                    DisplayCourse();
+                }
+            }
+
+        }
+
+        static void AddStudent()
+        {
+            {
+                Console.WriteLine("Enter the course name");
+                string coursename = Console.ReadLine();
+                CourseStudentDataAccess db = new CourseStudentDataAccess();
+                int courseid = db.FindCourse(coursename);
+                if (courseid == 0)
+                {
+                    Console.WriteLine("No such course exists");
+                }
+                else
+                {
+                    Student student = new Student();
+                    Console.WriteLine("Enter student name");
+                    student.name = Console.ReadLine();
+
+                    Console.WriteLine("Enter student marks");
+                    string marks = Console.ReadLine();
+                    int markschecked = 0;
+                    if (int.TryParse(marks, out markschecked) == true) // Try parse return
+                    {
+                        student.marks = markschecked;
+                        student.courseId = courseid;
+                        db.SaveStudent(student);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Marks should be numeric");
+
+                    }
+
+
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+        static void AddNewCourse()
+        {
+            {
+                Course course = new Course();
+                Console.WriteLine("Enter Course name");
+                course.name = Console.ReadLine();
+                // course.Add(Course); - remove to make the dataaccess call below
+                CourseStudentDataAccess db = new CourseStudentDataAccess(); // create object of this
+                db.SaveCourse(course);
+
+                Console.ReadLine();
+            }
+        }
+
+        static void DisplayOptions()
+        {
+            Console.WriteLine("Press (1) - Add a new course.");
+            Console.WriteLine("Press (2) - Add student to course.");
+            Console.WriteLine("Press (3) - Display all courses, total students, average marks");
+            Console.WriteLine("Press (4) - Display all students : Student ID, Name, Marks");
+            Console.WriteLine("Press (5) - To Exit");
+
+            enter = Convert.ToChar(Console.ReadLine());
+        }
+
+        static void DisplayOptionfour()
+        {
+            foreach (Student attributes in students)
+            {
+                Console.WriteLine(attributes.id + " " + attributes.name);
+            }
+        }
+
+        static void DisplayCourse()
+        {
+            CourseStudentDataAccess db = new CourseStudentDataAccess(); //create instances
+
+            List<Course> courses = db.getCourses();
+
+            foreach (var temp in courses) // coming from sql server
+            {
+                Console.WriteLine("Id :- " + temp.id
+                            + " Course Name :- " + temp.name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ContosoConsoleUI - Faris/ContosoDataAccess/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using ContosoModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace ContosoDataAccess
+{
+    public class CourseStudentDataAccess
+    {
+        private string connectionString = "Data Source=ZENBOOK-UX303LA\MSSQLSERVER01;Initial Catalog=ContosoConsoloDB;Integrated Security=True";
+        public bool SaveCourse(Course obj)
+        {
+            // Step 1 :- open the connection  ado.net, (active data objective)
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            // Step 2 :- Create SQL insert into
+            SqlCommand comm = new SqlCommand();
+            comm.CommandText = "insert into tblCourse(coursename) values('" + obj.name + "')";
+            comm.Connection = conn;
+            // Step 3 :- Execute query
+            comm.ExecuteNonQuery(); // means i don't want to receive it but insert it
+            // Step 4 :- Close connection 
+            conn.Close();
+            return true;
+        }
+        public bool SaveStudent(Student obj)
+        {
+            // Step 1 :- open the connection
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            // Step 2 :- Create SQL insert into.
+            SqlCommand comm = new SqlCommand();
+            comm.CommandText = "insert into tblstudent(studentname,studentmarks,courseid_fk) values('"
+                                + obj.name + "'," + obj.marks + "," + obj.courseId + ")";
+            comm.Connection = conn;
+            // Step 3 :- Execute query
+            comm.ExecuteNonQuery();
+            // Step 4 :- Close connection 
+            conn.Close();
+            return true;
+        }
+        public int FindCourse(string courseName)
+        {
+            List<Course> courses = new List<Course>();
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            SqlCommand comm = new SqlCommand();
+            comm.CommandText = "select Id from tblCourse where CourseName='" + courseName + "'"; //double quote for c# and single quote for sqlquery
+            comm.Connection = conn;
+            SqlDataReader dr = comm.ExecuteReader();
+            dr.Read(); // go to first row and get the id
+            int id = Convert.ToInt32(dr["id"]);
+            conn.Close();
+            return id;
+
+
+        }
+        public List<Course> getCourses()
+        {
+            // Create instances of courses
+            List<Course> courses = new List<Course>();
+            // Step 1 :- open the connection
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            // Step 2:: Create SQL insert into.
+            SqlCommand comm = new SqlCommand();
+            comm.CommandText = "select id,CourseName from tblCourse"; //sql not case sensitive
+            comm.Connection = conn;
+            //Step 3: Execute Reader and do the function reading
+            SqlDataReader dr = comm.ExecuteReader();
+            while (dr.Read())
+            {
+                Course temp = new Course(); //create a course object 
+                temp.id = Convert.ToInt32(dr["id"]); // extract id data from sql into object
+                temp.name = dr["CourseName"].ToString(); // extract id data from sql filling into object
+                courses.Add(temp); // add to list
+            }
+
+            //Step 4: Close connection
+            conn.Close(); // always close first before do a return
+
+            return courses; //return to UI
+        }
+    }
+}
